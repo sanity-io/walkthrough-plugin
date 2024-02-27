9a279299f62bb9876b55024d05d6102bf899240b
Drew Lyton <lytle.drew@gmail.com>
2024-02-27 14:58:35 -0500

    add type for walkthrough and basic sidebar data rendering

--- src/ActiveToolLayout.tsx
+++ src/ActiveToolLayout.tsx
@@ -1,13 +1,25 @@
 import {Box, Card, Flex, Text} from '@sanity/ui'
-import {ActiveToolLayoutProps} from 'sanity'
+import {ActiveToolLayoutProps, useProjectId} from 'sanity'
+import useSWR from 'swr'
+import {getWalkthrough} from './data/getWalkthrough'
+import pluginVersion from './pluginVersion'
 
 export function SidebarPanel() {
+  const projectId = useProjectId()
+  const {data} = useSWR(`/walkthrough/${projectId}`, () =>
+    getWalkthrough({projectId, pluginVersion}),
+  )
+
+  if (!data) return null
   return (
     <Card padding={[3, 3, 4]} radius={2} shadow={1} tone="primary">
-      <Text size={4} style={{paddingBottom: '3em'}}>
-        Get Started
+      <Text size={2} style={{paddingBottom: '3em'}}>
+        {data.flow.overline}
       </Text>
-      <Text>This is a right sidebar. </Text>
+      <Text size={4} style={{paddingBottom: '3em'}}>
+        {data.flow.header}
+      </Text>
+      {data && <pre>{JSON.stringify({steps: data.flow.steps})}</pre>}
     </Card>
   )
 }
--- src/data/types.ts
+++ src/data/types.ts
@@ -1,3 +1,20 @@
+import {PortableTextBlock} from 'sanity'
+
 export type Walkthrough = {
   _id: string
+  completedSteps: string[]
+  pluginVersion: string
+  flow: {
+    _id: string
+    name: string
+    header: string
+    overline: string
+    steps: {
+      _id: string
+      title: string
+      icon: string
+      badge: string
+      content: PortableTextBlock
+    }
+  }
 }
