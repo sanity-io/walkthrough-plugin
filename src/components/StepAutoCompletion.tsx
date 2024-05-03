d72da0e54471cad6801d0e4530c32eb3800571f2
Drew Lyton <lytle.drew@gmail.com>
2024-05-03 12:54:09 -0400

    feat: detect CLI ejection step

--- src/components/StepAutoCompletion.tsx
+++ src/components/StepAutoCompletion.tsx
@@ -1,8 +1,10 @@
+import {useEjected} from './useEjected'
 import {useInvitedMembers} from './useInvitedMember'
 import {usePublishedDocument} from './usePublishedDocument'
 
 export function StepAutoCompletion({onStepCompletion}: {onStepCompletion: () => void}) {
   usePublishedDocument(onStepCompletion)
   useInvitedMembers(onStepCompletion)
+  useEjected(onStepCompletion)
   return null
 }
---
+++ src/components/useEjected.ts
@@ -0,0 +1,45 @@
+import {useClient} from 'sanity'
+import {useStep} from './StepItem'
+import useSWR from 'swr'
+import {useCallback, useEffect} from 'react'
+import {useTelemetry} from '@sanity/telemetry/react'
+import {QuickstartStepCompleted} from '../data/telemetry'
+
+export function useEjected(toggleComplete: () => void) {
+  //   Use the sanity client to determine whether we're on a staging
+  //  or production project
+  const client = useClient()
+  const isStaging = client.getUrl('').includes('work')
+
+  const {stepName, isComplete, projectId, stepId} = useStep()
+  const telemetry = useTelemetry()
+
+  const {data, isLoading} = useSWR(
+    'project-cli-ejected',
+    () =>
+      fetch(`https://api.sanity.${isStaging ? 'work' : 'io'}/v1/projects/${projectId}`, {
+        credentials: 'include',
+      }).then((resp) => resp.json()),
+    //   Poll the endpoint every 5 minutes
+    {refreshInterval: 300000},
+  )
+  const isEjected = data?.metadata?.cliInitializedAt
+
+  const markComplete = useCallback(() => {
+    if (!isComplete) {
+      toggleComplete()
+      // Run tracking
+      telemetry.log(QuickstartStepCompleted, {
+        projectId,
+        stepId,
+        stepName,
+      })
+    }
+  }, [telemetry, isComplete, stepId, stepName, projectId, toggleComplete])
+
+  useEffect(() => {
+    if (stepName === 'Continue development locally with CLI' && !isLoading && isEjected) {
+      markComplete()
+    }
+  }, [stepName, isLoading, isEjected, markComplete])
+}
